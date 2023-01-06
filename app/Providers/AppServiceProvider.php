9560c5048de2e0e8544be018ba571272ab5c45fb
sangsakaa <zainuri717@gmail.com>
2023-01-06 10:55:13 +0700

    npm boot schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 
@@ -24,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+        Carbon::setLocale('id');
     }
 }
